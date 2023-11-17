84400be3a0418df7457de46815dfc6edbb1a4a72
Bram Laros <bramlaros@gmail.com>
2023-11-17 11:26:19 +0100

    Changed servo to stepper motor

--- DesignChallenge_LockSystem/src/main.cpp
+++ DesignChallenge_LockSystem/src/main.cpp
@@ -1,71 +1,50 @@
 #include <Arduino.h>
-#include <Servo.h>
-#include <AccelStepper.h>
+#include <Stepper.h>
 
 /*
 Pin initialisatie actuatoren en sensoren.
 Initialisatie variabelen
 */
 
-int ledPinRood = 7;
-int ledPinGroen = 9;
-int servoPin = 6;
+int ledPinRood = 4;
 int knopPinGroen = 5;
-int knopPinRood = 8;
-int buzzerPin = 12;
+int knopPinRood = 6;
+int buzzerPin = 7;
 
-int fietsTrigPin = 3;
-int fietsEchoPin = 2;
-int spaakTrigPin = 11;
-int spaakEchoPin = 10;
+int fietsTrigPin = 12;
+int fietsEchoPin = 13;
 
 int maxAfstandFiets = 11;
-int maxAfstandSpaak = 7;
-int servoDegree = 0;
 
 int buttonStateGroen;
 int buttonStateRood;
 int gemetenAfstandFiets;
-int gemetenAfstandSpaak;
 
 long duration;
 int distance;
 bool ifPressed = false;
-Servo myservo;
 
 int motorPin1 = 8;
 int motorPin2 = 9;
 int motorPin3 = 10;
 int motorPin4 = 11;
 
-int stepsPerRevolution = 64;
-int degreePerRevolution = 5.625;
-
-AccelStepper stepper(AccelStepper::HALF4WIRE, motorPin1, motorPin2, motorPin3, motorPin4);
-
-float degToSteps(float deg){
-  return (stepsPerRevolution / degreePerRevolution) * deg; 
-}
+Stepper stepper(2048, motorPin1, motorPin3, motorPin2, motorPin4);
 
 void setup() {
   Serial.begin(9600);
-  stepper.setMaxSpeed(1000.0);
-  stepper.setAcceleration(100.0);
-  stepper.setSpeed(200);
-  stepper.moveTo(degToSteps(360));
-  // pinMode(ledPinRood, OUTPUT);
-  // pinMode(ledPinGroen, OUTPUT);
-  // pinMode(knopPinGroen, INPUT_PULLUP);
-  // pinMode(knopPinRood, INPUT_PULLUP);
-  // pinMode(buzzerPin, OUTPUT);
 
-  // pinMode(fietsTrigPin, OUTPUT);
-  // pinMode(fietsEchoPin, INPUT);
-  // pinMode(spaakTrigPin, OUTPUT);
-  // pinMode(spaakEchoPin, INPUT);
+  pinMode(ledPinRood, OUTPUT);
+  pinMode(knopPinGroen, INPUT_PULLUP);
+  pinMode(knopPinRood, INPUT_PULLUP);
+  pinMode(buzzerPin, OUTPUT);
 
-  // myservo.attach(servoPin);
-  // digitalWrite(ledPinRood, HIGH);
+  pinMode(fietsTrigPin, OUTPUT);
+  pinMode(fietsEchoPin, INPUT);
+
+  digitalWrite(ledPinRood, HIGH);
+
+  stepper.setSpeed(15);
 }
 
 
@@ -75,17 +54,18 @@ Function void startServo()
 Check of knop ingedrukt is voor draaien servo
 */
 
-// void startServo() {
-//   if (ifPressed) {
-//     myservo.write(140);
-//     digitalWrite(ledPinRood, HIGH);
-//     digitalWrite(ledPinGroen, LOW);
-//   } else {
-//     myservo.write(0); // Zet de servo in de neutrale positie (90 graden)
-//     digitalWrite(ledPinRood, LOW);
-//     digitalWrite(ledPinGroen, HIGH);
-//   }
-// }
+void startServo() {
+  if (ifPressed) {
+    Serial.println("on");
+    digitalWrite(ledPinRood, HIGH);
+    stepper.step(1000);
+    delay(100);
+  } else {
+    Serial.println("off");
+    stepper.step(-1000);
+    digitalWrite(ledPinRood, LOW);    
+  }
+}
 
 /*
 Function int meetAfstand()
@@ -95,17 +75,17 @@ Echo = meegegeven echoPin
 Geeft vervolgens gemeten afstand terug
 */
 
-// int meetAfstand(int Trig, int Echo) {
-//   digitalWrite(Trig, LOW);
-//   delay(2);
-//   digitalWrite(Trig, HIGH);
-//   delay(100);
-//   digitalWrite(Trig, LOW);
-//   duration = pulseIn(Echo, HIGH);
+int meetAfstand(int Trig, int Echo) {
+  digitalWrite(Trig, LOW);
+  delay(2);
+  digitalWrite(Trig, HIGH);
+  delay(100);
+  digitalWrite(Trig, LOW);
+  duration = pulseIn(Echo, HIGH);
 
-//   distance = duration * 0.032 / 2;
-//   return distance;
-// }
+  distance = duration * 0.032 / 2;
+  return distance;
+}
 
 /*
 Function void loop()
@@ -113,38 +93,39 @@ Leest de buttonstates en afstanden
 */
 
 void loop() {
-  stepper.run();
-  // buttonStateGroen = digitalRead(knopPinGroen);
-  // buttonStateRood = digitalRead(knopPinRood);
-  // gemetenAfstandFiets = meetAfstand(fietsTrigPin, fietsEchoPin);
-  // gemetenAfstandSpaak = meetAfstand(spaakTrigPin, spaakEchoPin);
+  buttonStateGroen = digitalRead(knopPinGroen);
+  buttonStateRood = digitalRead(knopPinRood);
+  gemetenAfstandFiets = meetAfstand(fietsTrigPin, fietsEchoPin);
 
-  // if (buttonStateGroen == LOW && !ifPressed && gemetenAfstandFiets < maxAfstandFiets && gemetenAfstandSpaak > maxAfstandSpaak) {
-  //   ifPressed = true;
-  // }
-  //   else if (buttonStateRood == LOW && ifPressed)
-  // {
-  //   ifPressed = false;
-  // } 
+  if (buttonStateGroen == LOW && !ifPressed && gemetenAfstandFiets < maxAfstandFiets) 
+  {
+    ifPressed = true;
+    startServo();
+    
+  }
+    else if (buttonStateRood == LOW && ifPressed)
+  {
+    ifPressed = false;
+    startServo();
+  } 
   
-  // if (ifPressed && gemetenAfstandFiets > 11)
-  // {
-  //   // Zet de buzzer aan met een frequentie van 1000 Hz
-  // tone(buzzerPin, 1500);
-  // delay(500); // Wacht 1 seconde
+  if (ifPressed && gemetenAfstandFiets > 11)
+  {
+    // Zet de buzzer aan met een frequentie van 1000 Hz
+  tone(buzzerPin, 1500);
+  delay(500); // Wacht 1 seconde
 
-  // // Zet de buzzer aan met een frequentie van 2000 Hz
-  // tone(buzzerPin, 2000);
-  // delay(500); // Wacht 1 seconde
+  // Zet de buzzer aan met een frequentie van 2000 Hz
+  tone(buzzerPin, 2000);
+  delay(500); // Wacht 1 seconde
 
-  // // Stop het geluid
-  // noTone(buzzerPin);
-  // delay(200); // Wacht 1 seconde
-  // }
+  // Stop het geluid
+  noTone(buzzerPin);
+  delay(200); // Wacht 1 seconde
+  }
   
   
-  // Serial.println(gemetenAfstandSpaak);
-  // startServo();
-  // delay(500);
-}
+  Serial.println(gemetenAfstandFiets);
+  delay(500);
+ }
 
